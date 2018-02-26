e1edf30316aae6bdd85379ee57ef99a0ab8b029e
likeaboy <jrocky.wang@gmail.com>
2018-02-26 15:34:33 +0800

    divide dp to subcalsses

--- src/main/java/org/jrocky/advanced/free/demo/algorithm/dp/CoinProblem.java
+++ src/main/java/org/jrocky/advanced/free/demo/algorithm/dp/CoinProblem.java
@@ -1,7 +1,12 @@
-package org.jrocky.advanced.free.demo.algorithm;
+package org.jrocky.advanced.free.demo.algorithm.dp;
 
-public class DP {
-	
+/**
+ * 简单动归
+ * 硬币问题
+ * @author wangzhijie
+ *
+ */
+public class CoinProblem {
 	//1,3,5面值，凑11至少需要多少枚硬币
 	private static void coin1(int s,int[] coinVals){
 		//迭代
@@ -24,33 +29,9 @@ public class DP {
 		return -1;
 	}
 	
-	//LIS
-	public static void lis(){
-		
-	}
-	
 	public static void main(String[] args) {
 		int[] coinVals = new int[]{1,3,5};
 		coin1(23,coinVals);
 		System.out.println(d(23,coinVals));
 	}
-	
-	
-	
-	/*private static void coin(){
-		int[] coinVals = new int[]{1,3,5};
-		int min = 0;
-		int s = 11;
-		while(s > 0 ){
-			for(int j=coinVals.length-1;j>=0;j--){
-				if(s > coinVals[j]){
-					
-				}
-			}
-		}
-	}
-	
-	private static int d(int s,int min){
-		return min+1;
-	}*/
 }
---
+++ src/main/java/org/jrocky/advanced/free/demo/algorithm/dp/LIS.java
@@ -0,0 +1,65 @@
+package org.jrocky.advanced.free.demo.algorithm.dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 最长子序列
+ * @author wangzhijie
+ *
+ */
+public class LIS {
+	//LIS
+	//5,3,4,8,6,7
+	public static void lis(int[] A){
+//		List<List<Integer>> rst = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+		int maxLen = 1;
+		List<Integer> longest = null;
+		for(int i=0;i<A.length;i++){
+			tmp.add(A[i]);
+			if(i+1 > A.length-1) break;
+			if(A[i] > A[i+1]){
+				if(tmp.size() > maxLen) {
+					maxLen = tmp.size();
+					longest = tmp;
+				}
+//				rst.add(tmp);
+				tmp = new ArrayList<Integer>();
+			}
+		}
+		System.out.println(maxLen);
+		print(longest);
+	}
+	//错误的解法
+	/*public static void lis2(int[] A){
+		int[] d = new int[A.length];
+		int maxLen = 1;
+		
+		for(int i=0;i<A.length;i++){
+			d[i]=1;
+			for(int j=0;j<i;j++){
+				if(A[j] <= A[i] && d[j]+1>d[i]){
+					d[i]=d[j]+1;
+				}
+			}
+			if(d[i]>maxLen)maxLen=d[i];
+		}
+		System.out.println(maxLen);
+	} */
+	
+	private static void print(List<Integer> list){
+		for(Integer e : list)
+			System.out.print(e+" ");
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		int[] A = new int[]{5,3,4,8,6,7};
+//		int[] A = new int[]{1,3,5,5,6,9,2,7};
+		System.out.println("lis:");
+		lis(A);
+		/*System.out.println("lis2:");
+		lis2(A);*/
+	}
+}
