d69239ce2b67530d87fd67117b2f904e24a2c79b
likeaboy <jrocky.wang@gmail.com>
2018-02-28 10:33:41 +0800

    add alibaba interview

---
+++ src/main/java/org/jrocky/advanced/free/demo/alibaba/interview/CommonProblem.java
@@ -0,0 +1,14 @@
+package org.jrocky.advanced.free.demo.alibaba.interview;
+/**
+ * 
+ * @author wangzhijie
+ *	http://www.100mian.com/mianshi/alibaba/4853.html
+ */
+public class CommonProblem {
+
+	/**
+	 * A、B、C三个线程同时对一个文件进行读写操作，A、B负责写，C负责读，A写完一个文件需要10个小时，
+	 * B单独写完一个文件需要15小时，C单独读完这个文件需要15小时，
+	 * 问：不考虑A、B、C相互之间的影响，现在多少小时才能写满文件？
+	 */
+}
