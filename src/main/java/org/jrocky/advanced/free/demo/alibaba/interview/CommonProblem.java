bed232374ce1bb74a5c3a35853cb2d1270788854
likeaboy <jrocky.wang@gmail.com>
2018-02-28 14:02:17 +0800

    update

--- src/main/java/org/jrocky/advanced/free/demo/alibaba/interview/CommonProblem.java
+++ src/main/java/org/jrocky/advanced/free/demo/alibaba/interview/CommonProblem.java
@@ -7,8 +7,8 @@ package org.jrocky.advanced.free.demo.alibaba.interview;
 public class CommonProblem {
 
 	/**
-	 * A、B、C三个线程同时对一个文件进行读写操作，A、B负责写，C负责读，A写完一个文件需要10个小时，
-	 * B单独写完一个文件需要15小时，C单独读完这个文件需要15小时，
+	 * A、B、C三个线程同时对一个文件进行读写操作，A、B负责写，C负责读，A写完一个文件需要6个小时，
+	 * B单独写完一个文件需要10小时，C单独读完这个文件需要15小时，
 	 * 问：不考虑A、B、C相互之间的影响，现在多少小时才能写满文件？
 	 */
 }
